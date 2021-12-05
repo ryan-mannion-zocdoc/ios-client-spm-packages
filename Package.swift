584dff846f9839258f65d50723d8a24c89cc9912
Ryan Mannion <ryan.mannion@zocdoc.com>
2021-12-05 13:54:53 -0600

    Remove bundle target

--- Package.swift
+++ Package.swift
@@ -10,14 +10,13 @@ let package = Package(
         .library(name: "AWSCognitoAuth", targets: ["AWSCognitoAuth"]),
         .library(name: "AWSCore", targets: ["AWSCore"]),
         .library(name: "AWSCognitoIdentityProviderASF", targets: ["AWSCognitoIdentityProviderASF"]),
-        .library(name: "SFMCMarketingCloud", targets: ["MarketingCloudSDK", "MarketingCloudSDKBundle"]),
+        .library(name: "MarketingCloudSDK", targets: ["MarketingCloudSDK"]),
     ],
     targets: [
         .binaryTarget(name: "AWSCognitoAuth", path: "AWSCognitoAuth.xcframework"),
         .binaryTarget(name: "AWSCore", path: "AWSCore.xcframework"),
         .binaryTarget(name: "AWSCognitoIdentityProviderASF", path: "AWSCognitoIdentityProviderASF.xcframework"),
         .binaryTarget(name: "MarketingCloudSDK", path: "MarketingCloudSDK/MarketingCloudSDK.xcframework"),
-        .binaryTarget(name: "MarketingCloudSDKBundle", path: "MarketingCloudSDK/MarketingCloudSDK.bundle"),
     ]
 )
 
