d2ff64fbb6037605391d1a1cd1df915410357f1f
Ryan Mannion <ryan.mannion@zocdoc.com>
2021-12-05 19:34:46 -0600

    Another try

--- Package.swift
+++ Package.swift
@@ -10,22 +10,19 @@ let package = Package(
         .library(name: "AWSCognitoAuth", targets: ["AWSCognitoAuth"]),
         .library(name: "AWSCore", targets: ["AWSCore"]),
         .library(name: "AWSCognitoIdentityProviderASF", targets: ["AWSCognitoIdentityProviderASF"]),
-        .library(
-            name: "MarketingCloudSDK",
-            targets: [
-                "MarketingCloudSDK",
-                "MarketingCloudSDKBundle",
-            ]
-        ),
+        .library(name: "SFMC", targets: ["SFMC"]),
     ],
     targets: [
         .binaryTarget(name: "AWSCognitoAuth", path: "AWSCognitoAuth.xcframework"),
         .binaryTarget(name: "AWSCore", path: "AWSCore.xcframework"),
         .binaryTarget(name: "AWSCognitoIdentityProviderASF", path: "AWSCognitoIdentityProviderASF.xcframework"),
+
         .binaryTarget(name: "MarketingCloudSDK", path: "MarketingCloudSDK/MarketingCloudSDK.xcframework"),
         .target(
-            name: "MarketingCloudSDKBundle",
-            dependencies: [],
+            name: "SFMC",
+            dependencies: [
+                .target(name: "MarketingCloudSDK"),
+            ],
             path: "MarketingCloudSDK",
             exclude: [],
             sources: nil,
