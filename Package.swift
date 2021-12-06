cbc91992f5a95b24cccf4940c9dadae7649beff0
Ryan Mannion <ryan.mannion@zocdoc.com>
2021-12-05 19:13:35 -0600

    Add resource bundle

--- Package.swift
+++ Package.swift
@@ -10,13 +10,29 @@ let package = Package(
         .library(name: "AWSCognitoAuth", targets: ["AWSCognitoAuth"]),
         .library(name: "AWSCore", targets: ["AWSCore"]),
         .library(name: "AWSCognitoIdentityProviderASF", targets: ["AWSCognitoIdentityProviderASF"]),
-        .library(name: "MarketingCloudSDK", targets: ["MarketingCloudSDK"]),
+        .library(
+            name: "MarketingCloudSDK",
+            targets: [
+                "MarketingCloudSDK",
+                "MarketingCloudSDKBundle",
+            ]
+        ),
     ],
     targets: [
         .binaryTarget(name: "AWSCognitoAuth", path: "AWSCognitoAuth.xcframework"),
         .binaryTarget(name: "AWSCore", path: "AWSCore.xcframework"),
         .binaryTarget(name: "AWSCognitoIdentityProviderASF", path: "AWSCognitoIdentityProviderASF.xcframework"),
         .binaryTarget(name: "MarketingCloudSDK", path: "MarketingCloudSDK/MarketingCloudSDK.xcframework"),
+        .target(
+            name: "MarketingCloudSDKBundle",
+            dependencies: [],
+            path: "MarketingCloudSDK",
+            exclude: [],
+            sources: nil,
+            resources: [
+                .copy("MarketingCloudSDK.bundle"),
+            ]
+        ),
     ]
 )
 
